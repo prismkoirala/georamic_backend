8346634c4306ff789a46312b50ccf3377d25d48b
prismkoirala <prizamkoirala@gmail.com>
2025-07-08 13:29:28 -0500

    yoo naya ho 9

--- georamic_backend/settings.py
+++ georamic_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ztym03+=b4&o*vhmi-(l#_g#iys@*fzg@vbwi29i7^&wuxwfl$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
